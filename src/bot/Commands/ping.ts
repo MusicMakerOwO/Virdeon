c7f5db28293ca2602d832a5219c1f71ddf7a3955
Music Maker <joshuanejdl@gmail.com>
2025-02-20 01:06:25 -0600

    fix: Updated command format in ping

--- src/bot/Commands/ping.ts
+++ src/bot/Commands/ping.ts
@@ -5,12 +5,6 @@ export const command = new SlashCommandBuilder()
 .setName('ping')
 .setDescription('Replies with Pong!');
 
-export const locks = {
-	// microbase restrictions as normal
-}
-
 export async function execute(interaction: Interaction) {
-	await new Promise(resolve => setTimeout(resolve, 5000));
-	await interaction.defer().catch( () => {} );
 	return 'Pong!';
 }
