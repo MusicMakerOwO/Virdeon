7de48775a6ffae72fca7d2d982dbc214ab99c521
Music Maker <joshuanejdl@gmail.com>
2025-02-25 11:30:43 -0600

    feat: Debounce function utility

---
+++ src/GlobalUtils/Debounce.ts
@@ -0,0 +1,7 @@
+export default function Debounce(func: Function, delay: number) {
+	let timeout : NodeJS.Timeout;
+	return function (...args: any[]) {
+		clearTimeout(timeout);
+		timeout = setTimeout(() => func(...args), delay);
+	};
+}
