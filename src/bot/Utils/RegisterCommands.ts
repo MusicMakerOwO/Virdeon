89d7b43f8040faf16aaac3ac6f4c051aa515f712
Music Maker <joshuanejdl@gmail.com>
2025-02-25 11:31:46 -0600

    feat: Added commang registration (more work needed)

---
+++ src/bot/Utils/RegisterCommands.ts
@@ -0,0 +1,46 @@
+import config from "../../Config";
+import { Client } from "../../types";
+import Log from "../../GlobalUtils/Logs";
+import type { RESTPostAPIApplicationCommandsJSONBody } from "discord-api-types/v10";
+
+const PUBLIC_ROUTE = `https://discord.com/api/v10/applications/${config.APP_ID}/commands`;
+
+export default async function RegisterCommands(client: Client, silent = false) {
+
+	if (!silent) Log('INFO', 'Registering (/) commands...');
+
+	const commandNames = new Set<string>();
+	const PublicCommands : RESTPostAPIApplicationCommandsJSONBody[] = [];
+
+	const localCommands = [...client.commands.values()];
+	for (let i = 0; i < localCommands.length; i++) {
+		const command = localCommands[i];
+		const commandData = command.command.toJSON();
+
+		if (commandNames.has(commandData.name)) {
+			Log('WARN', `Command name "${commandData.name}" is already in use`);
+			continue;
+		}
+
+		commandNames.add(commandData.name);
+		PublicCommands.push(commandData);
+	}
+
+	const response = await fetch(PUBLIC_ROUTE, {
+		method: 'PUT',
+		headers: {
+			'Authorization': `Bot ${config.TOKEN}`,
+			'Content-Type': 'application/json'
+		},
+		body: JSON.stringify(PublicCommands)
+	});
+
+	const json = await response.json();
+
+	if (response.ok === false) {
+		Log('ERROR', json);
+		return;
+	}
+
+	if (!silent) Log('INFO', `Registered ${json.length} commands`);
+}
